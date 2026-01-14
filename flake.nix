c75c135f8cdec1343b5884f606f7a86cb2e9d39d
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2026-01-14 08:33:51 +0100

    feat: switch from nixos-unstable to nixos-stable
    This change happens because of issues with unstable. While these are not fixed, I will stay on stable

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "My configuration Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
