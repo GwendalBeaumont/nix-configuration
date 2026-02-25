360f1d94cc5b0c6bc071dcddfa2103034ddcebd1
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2026-02-25 14:06:23 +0100

    feat: update git config on cedar

--- home/cedar/git/default.nix
+++ home/cedar/git/default.nix
@@ -4,11 +4,26 @@
   programs.git = {
     settings = {
       user.email = "gwendal.beaumont@gmail.com";
-
-      "includeIf \"gitdir:~/Documents/twindevops/\"" = {
-        path = "~/.config/git/imt/.gitconfig";
-      };
     };
+
+    includes = [
+      {
+        condition = "gitdir:~/Documents/twindevops";
+        contents = {
+          user = {
+            email = "gwendal.beaumont@imt-atlantique.fr";
+            name = "Gwendal Beaumont";
+            signingKey = "DBF0167B74C46CA84C0D258851F454CFBA3AEA40";
+          };
+          commit = {
+            gpgSign = true;
+          };
+          gpg = {
+            format = "openpgp";
+          };
+        };
+      }
+    ];
 
     signing = {
       key = "847D7D2DB3796CB8789644E373692F251579041D";
