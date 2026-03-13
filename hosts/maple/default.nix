e6b321de5acbaf25d36177c7def9c9deba1b8553
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2026-03-13 15:06:29 +0100

    feat: extract nixos common host options
    This commit extracts options common to every potentially future NixOS computer I may have. Only specific options are left to specific host folders

--- hosts/maple/default.nix
+++ hosts/maple/default.nix
@@ -3,33 +3,18 @@
 {
   imports = [
     ./hardware-configuration.nix
+    ../common/nixos.nix
 
     "${modules}/containers"
     "${modules}/gnome"
     "${modules}/printing"
   ];
 
-  # Bootloader
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  boot.loader = {
-    systemd-boot = {
-      enable = true;
-      configurationLimit = 10;
-    };
-    efi = {
-      canTouchEfiVariables = true;
-    };
-  };
-
   boot.initrd.luks.devices."luks-8f74c7fd-732f-43c7-ba42-380c3188d00c".device = "/dev/disk/by-uuid/8f74c7fd-732f-43c7-ba42-380c3188d00c";
 
   # Networking
   networking = {
-    hostName = "${hostname}";
     networkmanager = {
-      enable = true;
-
       plugins = with pkgs; [
         networkmanager-openvpn
         networkmanager-openconnect
@@ -54,93 +39,4 @@
       };
     };
   };
-
-  # Set your time zone.
-  time.timeZone = "Europe/Paris";
-
-  # Select internationalisation properties.
-  i18n = {
-    defaultLocale = "en_GB.UTF-8";
-
-    extraLocaleSettings = {
-      LC_ADDRESS = "fr_FR.UTF-8";
-      LC_IDENTIFICATION = "fr_FR.UTF-8";
-      LC_MEASUREMENT = "fr_FR.UTF-8";
-      LC_MONETARY = "fr_FR.UTF-8";
-      LC_NAME = "fr_FR.UTF-8";
-      LC_NUMERIC = "fr_FR.UTF-8";
-      LC_PAPER = "fr_FR.UTF-8";
-      LC_TELEPHONE = "fr_FR.UTF-8";
-      LC_TIME = "en_DK.UTF-8";
-    };
-  };
-
-  # Power management for laptops
-  services.thermald.enable = true;
-
-  # Configure console keymap
-  console.keyMap = "fr";
-
-  # Enable sound with pipewire.
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  # Garbage collection
-  nix = {
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 1w";
-    };
-    settings.auto-optimise-store = true;
-  };
-
-  # User account. Don't forget to set a password with ‘passwd’.
-  users.users.${username} = {
-    isNormalUser = true;
-    description = "${username}";
-    extraGroups = [ "networkmanager" "wheel" ];
-    shell = pkgs.zsh;
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.settings.download-buffer-size = 524288000;
-  environment.systemPackages = with pkgs; [
-    git
-    vim
-  ];
-
-  # Env
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-  };
-
-  # Programs
-  programs.zsh.enable = true;
-
-  programs.gnupg.agent = {
-    enable = true;
-    pinentryPackage = pkgs.pinentry-gnome3;
-  };
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.pcscd.enable = true;
-
-  # Enable Linux Vendor Firmware Update
-  services.fwupd.enable = true;
-
-  # Deactivate documentation
-  documentation.nixos.enable = false;
-
-  system.stateVersion = "25.05";
 }
