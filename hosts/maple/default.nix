c4aaac9f2541bf17dbde0d838533e96b5d1f45c4
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2026-03-13 14:56:11 +0100

    feat: set username as user description

--- hosts/maple/default.nix
+++ hosts/maple/default.nix
@@ -103,7 +103,7 @@
   # User account. Don't forget to set a password with ‘passwd’.
   users.users.${username} = {
     isNormalUser = true;
-    description = "Gwendal";
+    description = "${username}";
     extraGroups = [ "networkmanager" "wheel" ];
     shell = pkgs.zsh;
   };
