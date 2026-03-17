077355541fd133e73501a6a852ff8becf08493bd
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2026-03-17 09:38:01 +0100

    feat: update printers configuration

--- modules/printing/default.nix
+++ modules/printing/default.nix
@@ -1,14 +1,20 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
-  config = {
-    services.printing = {
-      enable = true;
-      drivers = with pkgs; [
-        hplip
-        foomatic-db-ppds
-      ];
-    };
-    services.samba.enable = true;
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [
+      cups-filters
+      cups-browsed
+      hplip
+      foomatic-db-ppds
+    ];
   };
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+  services.samba.enable = true;
 }
