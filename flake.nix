84e73f1d38461c8b3646ac4f232dacd255d87114
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2026-05-22 09:54:57 +0200

    feat!: switch to nixos-unstable-small channel

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Entry Flake to configure my machines.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
     home-manager = {
       url = "github:nix-community/home-manager";
