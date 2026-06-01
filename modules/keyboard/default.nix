370493f17e0871d0757d9cbb18552d113feaf1b4
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2026-06-01 15:22:13 +0200

    chore: clean keyboard module

--- modules/keyboard/default.nix
+++ modules/keyboard/default.nix
@@ -1,11 +1,10 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Add QMK/VIA
-  environment.systemPackages = with pkgs; [ via qmk qmk-udev-rules ];
+  # Add QMK/VIA support
+  environment.systemPackages = with pkgs; [ via qmk ];
   hardware.keyboard.qmk.enable = true;
-  services.udev.packages = [ pkgs.qmk-udev-rules pkgs.via ];
 
   # Add udev rule for MonsGeek M1 V5 ISO
-  services.udev.extraRules = ''KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="342d", ATTRS{idProduct}=="e4c2", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"'';
+  services.udev.extraRules = ''KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="342d", ATTRS{idProduct}=="e4c2", MODE="0666", GROUP="users", TAG+="uaccess", TAG+="udev-acl"'';
 }
