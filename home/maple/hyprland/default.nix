016c746627b7716a948dcf0bc74b5592602bfaab
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2026-06-27 21:59:05 +0200

    feat: add hypridle

--- home/maple/hyprland/default.nix
+++ home/maple/hyprland/default.nix
@@ -6,6 +6,7 @@ with lib; {
     ./conf
 
     # Programs
+    ./hypridle.nix
     ./hyprlock.nix
   ];
 
---
+++ home/maple/hyprland/hypridle.nix
@@ -0,0 +1,30 @@
+{ inputs, config, lib, pkgs, ... }:
+
+{
+  services.hypridle = {
+    enable = true;
+
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch 'hl.dsp.dpms({ action = \"enable\" })'";
+      };
+
+      listener = [
+        {
+          # Lock
+          timeout = 300; # 5 mins
+          on-timeout = "hyprlock";
+        }
+
+        {
+          # Turn off screen
+          timeout = 360; # 6 mins
+          on-timeout = "hyprctl dispatch 'hl.dsp.dpms({ action = \"disable\" })'";
+          on-resume = "hyprctl dispatch 'hl.dsp.dpms({ action = \"enable\" })'";
+        }
+      ];
+    };
+  };
+}
