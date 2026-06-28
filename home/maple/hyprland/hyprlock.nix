c6356d3e4decae2b32ff4174cf55127c769108ed
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2026-06-28 14:33:48 +0200

    feat: complete hyprlock settings

--- home/maple/hyprland/hyprlock.nix
+++ home/maple/hyprland/hyprlock.nix
@@ -5,6 +5,7 @@ with lib; {
     enable = true;
     settings = {
       general = {
+        hide_cursor = true;
         ignore_empty_input = true;
       };
 
@@ -23,35 +24,35 @@ with lib; {
       };
 
       input-field = {
-        size = "20%, 5%";
+        size = "10%, 3%";
         outline_thickness = 3;
         inner_color = "rgba(0, 0, 0, 0.0)"; # no fill
 
-        outer_color = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        outer_color = "rgba(255,255,255,1)";
         check_color = "rgba(00ff99ee) rgba(ff6633ee) 120deg";
         fail_color = "rgba(ff6633ee) rgba(ff0066ee) 40deg";
 
         font_color = "rgb(255, 255, 255)";
         fade_on_empty = false;
-        rounding = 15;
+        rounding = 30;
 
         placeholder_text = "Input password...";
         fail_text = "$PAMFAIL$FPRINTFAIL";
 
-        position = "0, -20";
+        position = "0, 10%";
         halign = "center";
-        valign = "center";
+        valign = "bottom";
       };
 
       label = [
         # TIME
         {
           text = "$TIME"; # ref. https://wiki.hyprland.org/Hypr-Ecosystem/hyprlock/#variable-substitution
-          font_size = 90;
+          font_size = 80;
 
-          position = "-30, 0";
-          halign = "right";
-          valign = "top";
+          position = "0, 200";
+          halign = "center";
+          valign = "center";
         }
 
         # DATE
@@ -59,9 +60,9 @@ with lib; {
           text = "cmd[update:60000] date +\"%A, %d %B %Y\""; # update every 60 seconds
           font_size = 25;
 
-          position = "-30, -150";
-          halign = "right";
-          valign = "top";
+          position = "0, 130";
+          halign = "center";
+          valign = "center";
         }
       ];
     };
