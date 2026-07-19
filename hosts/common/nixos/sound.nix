e82d095b8451880a6a717304309bc6f81658c449
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2026-07-19 14:28:58 +0200

    feat: AirPlay

--- hosts/common/nixos/sound.nix
+++ hosts/common/nixos/sound.nix
@@ -7,5 +7,22 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    raopOpenFirewall = true;
+
+    extraConfig.pipewire = {
+      "10-airplay" = {
+        "context.modules" = [
+          {
+            name = "libpipewire-module-raop-discover";
+
+            # increase the buffer size if you get dropouts/glitches
+            # args = {
+            #   "raop.latency.ms" = 500;
+            # };
+          }
+        ];
+      };
+    };
   };
 }
