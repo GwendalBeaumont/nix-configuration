7ed6e172203ca2239bc62ca674afa3825253b921
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2026-07-19 14:33:19 +0200

    fix: hyprland

--- home/maple/hyprland/conf/autostart.nix
+++ home/maple/hyprland/conf/autostart.nix
@@ -2,12 +2,14 @@
 
 {
   wayland.windowManager.hyprland.settings = {
-    on = {
-      _args = [
-        "hyprland.start"
-        # Make sure to start waybar after nm-applet to avoid glitched icon
-        (lib.generators.mkLuaInline "function()\n  hl.exec_cmd(\"systemctl --user start hyprpolkitagent\")\n  hl.exec_cmd(\"nm-applet\")\n  hl.exec_cmd(\"waybar\")\nend")
-      ];
-    };
+    on = [
+      {
+        _args = [
+          "hyprland.start"
+          # Make sure to start waybar after nm-applet to avoid glitched icon
+          (lib.generators.mkLuaInline "function()\n  hl.exec_cmd(\"systemctl --user start hyprpolkitagent\")\n  hl.exec_cmd(\"nm-applet\")\n  hl.exec_cmd(\"waybar\")\nend")
+        ];
+      }
+    ];
   };
 }
