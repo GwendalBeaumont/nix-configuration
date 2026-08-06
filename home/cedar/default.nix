1d99791c4b4d9d02827d5da0963d2d66fbb6ce4f
Gwendal Beaumont <gwendal.beaumont@gmail.com>
2026-08-06 11:43:34 +0200

    update

--- home/cedar/default.nix
+++ home/cedar/default.nix
@@ -16,16 +16,16 @@
   programs = {
     direnv.enable = true;
     nix-direnv.enable = true;
-    fastfetch.enable = true;
+    # fastfetch.enable = true;
     git.enable = true;
     gpg.enable = true;
     kitty = {
       enable = true;
       package = pkgs.emptyDirectory;
     };
-    lazygit.enable = true;
-    lf.enable = true;
-    neovim.enable = true;
-    starship.enable = true;
+    # lazygit.enable = true;
+    # lf.enable = true;
+    # neovim.enable = true;
+    # starship.enable = true;
   };
 }
